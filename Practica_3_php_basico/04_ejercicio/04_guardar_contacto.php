a15671a7bc7c72a67b058f0c4a6256426497393a
IsmaelAguado <114136399+IsmaelAguado@users.noreply.github.com>
2022-11-22 23:32:49 +0100

    Add files via upload

---
+++ Practica_3_php_basico/04_ejercicio/04_guardar_contacto.php
@@ -0,0 +1,44 @@
+<?php
+
+    $nombre = trim(strip_tags($_REQUEST["nombre"]));
+    $trabajo = trim(strip_tags($_REQUEST["trabajo"]));
+    $telf = trim(strip_tags($_REQUEST["telefono"]));
+    $dir = trim(strip_tags($_REQUEST["direccion"]));
+    $otras = trim(strip_tags($_REQUEST["otras"]));
+    $nombre_archivo = "contactos.txt";
+
+    if ($nombre == "") {
+        print "El campo nombre esta vacio, es obligatorio";
+        print "<br><br>";
+        print "<a href='04_crear_contacto.html'><button>Volver</button></a>";
+    }
+    elseif ($trabajo == "") {
+        print "El campo trabajo esta vacio, es obligatorio";
+        print "<br><br>";
+        print "<a href='04_crear_contacto.html'><button>Volver</button></a>";
+    } 
+    elseif ($telf == "") {
+        print "El campo teléfono esta vacio, es obligatorio";
+        print "<br><br>";
+        print "<a href='04_crear_contacto.html'><button>Volver</button></a>";
+    } 
+    elseif ($dir == "") {
+        print "El campo Dirección esta vacio, es obligatorio";
+        print "<br><br>";
+        print "<a href='04_crear_contacto.html'><button>Volver</button></a>";
+    } 
+    elseif ($otras == "") {
+        print "El campo Otras esta vacio, es obligatorio";
+        print "<br><br>";
+        print "<a href='04_crear_contacto.html'><button>Volver</button></a>";
+    }
+ 
+
+else {
+    $contactos = fopen($nombre_archivo, "a");
+    fwrite($contactos, "$nombre:$trabajo:$telf:$dir:$otras" . PHP_EOL);
+    fclose($contactos);
+    print "Guardado con exito <br>";
+    print "<a href='04_agenda_contactos.html'><button>Volver</button></a>";
+
+}
